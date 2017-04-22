97f464d9f2fa495385b8058ba40ea9974baecd53
Sean Naber <seannaber@gmail.com>
2017-04-22 15:59:02 -0400

    js script link fix

--- js/script.js
+++ js/script.js
@@ -4,39 +4,49 @@ var cloudName = "imgprocessor";
 var apiKey = "188122967588527";
 var apiSecret = "olUTfOrPXdq2JxH-XxCIHEZmHdg";
 var presetName = "gtepwlbv";
+var approvedList = [];
+var approvedWebList = [];
+var rejectedList = [];
 
 $.cloudinary.config({ cloud_name: cloudName, api_key: apiKey});
 
 cloudinary.setCloudName(cloudName);
+cloudinary.setAPIKey(apiKey);
 
 openWidget = function() {
 cloudinary.openUploadWidget(
 	{upload_preset: presetName,
-	 folder: "testing",
+	 folder: "testing2",
 	 theme: "white",
-	 effect: "trim"}, 
+	 effect: "trim",
+	 max_image_width: 2500,
+	 max_image_height: 2500}, 
   function(err,d) {
+  	console.log(d);
   	if (err) {
+  		$("#errorContent").html("");
   		msg += "<h1>Error</h1>"
   		msg += err.message;
   		msg += "<p>Images over 1MB need not be checked</p>"
-  		$("#error").show().append(msg);
+  		$("#error").show();
+  		$("#errorContent").append(msg);
   	}
   	if ($("#smallButton").css("display") != "none") {
   	} else {
 	  	$("#bigButton").toggle();
 	  	$("#smallButton").toggle();
   	}
-  	console.log(err)
   	var msg = "";
   	$("#results").text(msg);
+  	console.log(d);
   	for (i=0;i<d.length;i++) {
-  		if (d[i].height < 1050) {
-  			msg += "<div class='resultDivFail col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1'>"	
-	  		msg += "<img class='resultImage' src=";
+  		if (d[i].height < 1050 && d[i].width < 1050) {
+  			rejectedList.push(d[i].public_id);
+  			msg += "<div class='resultDivFail col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1 tooltipped' data-position=top data-delay=50 data-tooltip='Do not submit. Image will be rejected'>"	
+	  		msg += "<div class='imgWrapper valign-wrapper'><img class='resultImage' src=";
 	  		msg += d[i].url;
-	  		msg += ">";
-	  		msg += "<p><a class='fail btn-floating btn-large waves-effect waves-light red darken-2 pulse'><i class=material-icons>cancel</i></a></span>"
+	  		msg += "></div>";
+	  		msg += "<a class='fail btn-floating btn-large waves-effect waves-light red darken-2 pulse'><i class=material-icons>cancel</i></a></span></p>"
 	  		msg += "<h2 class='fileName truncate'>" + d[i].original_filename + "." + d[i].format + "</h2>";
 	  		msg += "<div class=chip>";
 	  		msg += "Height: " + d[i].height + "</div>";
@@ -48,12 +58,13 @@ cloudinary.openUploadWidget(
   			msg += "<p class=details>Image meets " + ((d[i].height/1050)*100).toFixed() + "%</br>of web requirements</p>"
 	  		msg += "</div>";
 
-  		} else if (d[i].height < 1800) {
-			msg += "<div class='resultDivSuccessWeb col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1'>"	
-	  		msg += "<img class='resultImage' src=";
+  		} else if (d[i].height < 1800 && d[i].width < 1800) {
+  			approvedWebList.push(d[i].public_id);
+			msg += "<div class='resultDivSuccessWeb col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1 tooltipped' data-position=top data-delay=50 data-tooltip='This image will be approved for web use only'>"	
+	  		msg += "<div class='imgWrapper valign-wrapper'><img class='resultImage' src=";
 	  		msg += d[i].url;
-	  		msg += ">";
-	  		msg += "<p><a class='successWeb btn-floating btn-large waves-effect waves-light orange darken-1'><i class=material-icons>warning</i></a></span>"
+	  		msg += "></div>";
+	  		msg += "<a class='successWeb btn-floating btn-large waves-effect waves-light orange darken-1'><i class=material-icons>warning</i></a></span>"
 	  		msg += "<h2 class='fileName truncate'>" + d[i].original_filename + "." + d[i].format + "</h2>";
 	  		msg += "<div class=chip>";
 	  		msg += "Height: " + d[i].height + "</div>";
@@ -65,11 +76,14 @@ cloudinary.openUploadWidget(
   			msg += "<p class=details>Image meets " + ((d[i].height/1800)*100).toFixed() + "%</br>of print requirements</p>"
 	  		msg += "</div>";
   		} else {
-  			msg += "<div class='resultDivSuccess col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1'>"	
-	  		msg += "<img class='resultImage' src=";
+  			approvedList.push(d[i].public_id);
+  			var siggy = d[i].signature;
+  			console.log(siggy);
+  			msg += "<div class='resultDivSuccess col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1 tooltipped' data-position=top data-delay=50 data-tooltip='This image will be approved for all uses'>"	
+	  		msg += "<div class='imgWrapper valign-wrapper'><img class='resultImage' src=";
 	  		msg += d[i].url;
-	  		msg += ">";
-	  		msg += "<p><a class='success btn-floating btn-large waves-effect waves-light green lighten-1'><i class=material-icons>done</i></a></span>"
+	  		msg += "></div>";
+	  		msg += "<a class='success btn-floating btn-large waves-effect waves-light green lighten-1'><i class=material-icons>done</i></a></span>"
 	  		msg += "<h2 class='fileName truncate'>" + d[i].original_filename + "." + d[i].format + "</h2>";
 	  		msg += "<div class=chip>";
 	  		msg += "Height: " + d[i].height + "</div>";
@@ -85,6 +99,38 @@ cloudinary.openUploadWidget(
   	}
   	$("#results").append(msg);
   	Materialize.fadeInImage('img');
+  	$('.tooltipped').tooltip({delay: 50});
+  	console.log(approvedList);
+
+  	var idsList = "";
+  	for (var i=0; i<approvedList.length; i++) {
+  		idsList += "&public_ids%5B%5D=";
+  		idsList += approvedList[i];
+  	}
+
+  	console.log(idsList);
+
+  	var timestamp = (Date.now() / 1000).toFixed(0);
+
+  	console.log(timestamp);
+
+  	var signature = CryptoJS.SHA1("mode=create&public_ids=" + approvedList.join(",") + "&timestamp=" + timestamp + "olUTfOrPXdq2JxH-XxCIHEZmHdg").toString(CryptoJS.enc.Hex);
+
+  	console.log(signature);
+  	
+	$.getJSON("https://api.cloudinary.com/v1_1/imgprocessor/image/generate_archive?&api_key=188122967588527&mode=create" + idsList + "&signature=" + signature + "&timestamp=" + timestamp,function(d,err) {
+  		console.log(d.secure_url);
+  		var msg = "";
+  		msg += "<div><a href=";
+  		msg += d.secure_url;
+  		msg += "><button id=downloadBtn class='waves-effect waves-light btn-large'>Download ZIP of all approved images</button></a></div>";
+  		$("#results").prepend(msg);
+  		// $("#downloadZip").toggle();
+  	});
+	// got hash from this string: mode=create&public_ids=testing2/tdr2qbkn3givxn7vs28c,sample&timestamp=1492805286olUTfOrPXdq2JxH-XxCIHEZmHdg
+	// Steps: 1) get current timestamp, 2) convert above string to SHA1 and use as signature
+	// to convert string to hash: CryptoJS.SHA1("mode=create&public_ids=testing2/tdr2qbkn3givxn7vs28c,sample&timestamp=1492801416olUTfOrPXdq2JxH-XxCIHEZmHdg").toString(CryptoJS.enc.Hex);
+
   })};
 
 $(".dropdown-button").dropdown();
@@ -107,6 +153,9 @@ function screenHeight () {
 function openHowTo() {
 	$("#howTo").toggle();
 }
+
+
+
 
 
 });
